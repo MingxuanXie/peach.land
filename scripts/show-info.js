1993256fd92027419c5eac5c277f05fd9644518a
Mingxuan Xie <31252716+MingxuanXie@users.noreply.github.com>
2021-02-27 20:56:58 +0800

    Create show-info.js

---
+++ scripts/show-info.js
@@ -0,0 +1,21 @@
+// Copyright © 2021 Mingxuan Xie (mingxuan.fun)
+//  1. create the info a-text with id="info" inside a camera entity
+//  2. put show-info="#info" inside the target entity
+
+AFRAME.registerComponent('show-info', {
+    schema: {type: 'string', default: ''},
+
+    init: function(){
+      var data = this.data;
+      var io = document.querySelector(data);
+      var el = this.el;
+      el.addEventListener('raycaster-intersected',function(){
+        io.object3D.visible = true;
+        // console.log("hovered") 
+      });
+      el.addEventListener('raycaster-intersected-cleared',function(){
+        io.object3D.visible = false;
+        // console.log("leaved") 
+      });
+     }
+});  
